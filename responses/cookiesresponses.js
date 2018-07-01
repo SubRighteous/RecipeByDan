24e284c56cdd9d497c5e1da50ac512f1832f2b53
SubRighteous <SubRighteous@users.noreply.github.com>
2018-07-01 11:41:50 -0400

    Added Example Recipe Respones

---
+++ responses/cookiesresponses.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const {ssml} = require('./utils');
+
+
+const CookiesRecipe = {
+    'Name' : ssml`
+    <speak>
+        Chewy Chocolate Chip Cookies
+    </speak>
+    `,
+    
+    'leadIntoRecipe': ssml`
+    <speak>
+        Ok, Getting the test version of Chewy Chocolate Chip Cookies.
+        <break time="1s" strength="weak"/>
+        Please Stand By.
+        <break time="200ms" strength="weak"/>
+         <audio src="https://firebasestorage.googleapis.com/v0/b/recipebydan.appspot.com/o/audio%2FAssistantBeamUp.ogg?alt=media">
+            Broken Plate
+        </audio>
+        <break time="200ms" strength="weak"/>
+        When you are ready, just say
+        <break time="200ms" strength="weak"/> 
+        tell me the ingredients. 
+        <break time="1s" strength="weak"/>
+        You can also say, tell me the directions. 
+    </speak>
+    `,
+    
+    'Directions' : ssml`
+    <speak>
+        Currently there are no Directions.
+        <break time="200ms" strength="weak"/>
+        Will be added soon.
+        <break time="100ms" strength="weak"/>
+        Dont worry about it.
+    </speak>
+    `,
+    
+    'Ingredients' : ssml`
+    <speak>
+        Currently there are no Ingredients.
+        <break time="200ms" strength="weak"/>
+        Sorry.
+    </speak>
+    `,
+    
+    'Step 1' : ssml`
+    <speak>
+        Step 1, Get a cookbook.
+    </speak>
+    `,
+    
+    'Step 2' : ssml`
+    <speak>
+        Step 2, read the cookbook.
+    </speak>
+    `,
+    
+};
+
+
+module.exports = {CookiesRecipe};
